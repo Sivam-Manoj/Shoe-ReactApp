e16526440bd59ac99507ca8009f53445530dd0c2
Sivam-Manoj <manom8193@gmail.com>
2023-12-28 16:20:35 +0530

    first commit

---
+++ src/Sidebar/Category/Category.js
@@ -0,0 +1,51 @@
+import Inputs from "../../Component/Input";
+import "./Category.css";
+
+
+function Category({handleChange}) {
+
+  return (
+
+    
+
+<div>
+      <h2 className="sidebar-title">Category</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        
+        <Inputs
+          handleChange={handleChange}
+          value="sneakers"
+          title="Sneakers"
+          name="test"
+        />
+        <Inputs
+          handleChange={handleChange}
+          value="flats"
+          title="Flats"
+          name="test"
+        />
+        <Inputs
+          handleChange={handleChange}
+          value="sandals"
+          title="Sandals"
+          name="test"
+        />
+        <Inputs
+          handleChange={handleChange}
+          value="heels"
+          title="Heels"
+          name="test"
+        />
+      </div>
+    </div>
+
+   
+  )
+}
+
+export default Category;
